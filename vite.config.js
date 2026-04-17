3a1ecb876379edfa50d43dc375493a746f8b1d85
Juri Wolf <Amerlander@users.noreply.github.com>
2026-04-17 20:04:14 +0200

    feat: add site build configuration to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -38,38 +38,52 @@ const vuePlugins = {
 }
 
 // Build configuration
-const buildConfig = {
-  target: 'es2018',
-  lib: {
-    entry: `${process.cwd()}/src/components/index.js`,
-    name: pkg.name,
-    fileName: 'umo-editor',
-  },
-  outDir: 'dist',
-  copyPublicDir: false,
-  minify: 'esbuild',
-  cssMinify: true,
-  rollupOptions: {
-    output: [
-      {
-        banner: copyright,
-        intro: `import './umo-editor.css'`,
-        format: 'es',
-      },
-    ],
-    external: [
-      'vue',
-      /^@vueuse\/.*/,
-      /^@tiptap\/.*/,
-      /^prosemirror-*/,
-      /^nzh\/.*/,
-      ...Object.keys(pkg.dependencies),
-    ],
-    onwarn(warning, warn) {
-      if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
-      warn(warning)
+const getBuildConfig = ({ mode }) => {
+  const isSiteBuild = mode === 'site'
+
+  if (isSiteBuild) {
+    return {
+      target: 'es2018',
+      outDir: 'dist',
+      copyPublicDir: false,
+      minify: 'esbuild',
+      cssMinify: true,
+    }
+  }
+
+  return {
+    target: 'es2018',
+    lib: {
+      entry: `${process.cwd()}/src/components/index.js`,
+      name: pkg.name,
+      fileName: 'umo-editor',
     },
-  },
+    outDir: 'dist',
+    copyPublicDir: false,
+    minify: 'esbuild',
+    cssMinify: true,
+    rollupOptions: {
+      output: [
+        {
+          banner: copyright,
+          intro: `import './umo-editor.css'`,
+          format: 'es',
+        },
+      ],
+      external: [
+        'vue',
+        /^@vueuse\/.*/,
+        /^@tiptap\/.*/,
+        /^prosemirror-*/,
+        /^nzh\/.*/,
+        ...Object.keys(pkg.dependencies),
+      ],
+      onwarn(warning, warn) {
+        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
+        warn(warning)
+      },
+    },
+  }
 }
 
 const cssConfig = {
@@ -93,17 +107,21 @@ const cssConfig = {
   },
 }
 
-export default defineConfig({
-  base: '/umo-editor',
-  plugins: [ReactivityTransform(), ...Object.values(vuePlugins)],
-  css: cssConfig,
-  build: buildConfig,
-  esbuild: {
-    drop: ['debugger'],
-  },
-  resolve: {
-    alias: {
-      '@': `${process.cwd()}/src`,
+export default defineConfig(({ mode }) => {
+  const isSiteBuild = mode === 'site'
+
+  return {
+    base: isSiteBuild ? '/' : '/umo-editor',
+    plugins: [ReactivityTransform(), ...Object.values(vuePlugins)],
+    css: cssConfig,
+    build: getBuildConfig({ mode }),
+    esbuild: {
+      drop: ['debugger'],
     },
-  },
+    resolve: {
+      alias: {
+        '@': `${process.cwd()}/src`,
+      },
+    },
+  }
 })
